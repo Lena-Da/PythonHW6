c4261bc8809e14cec4887711e07570a4a00c5b5f
Lena <len.dal@yandex.ru>
2022-09-24 00:07:24 +0300

    3task

--- 3task.py
+++ 3task.py
@@ -17,3 +17,4 @@ point_list=list(map(int,point_str))
 result=math.sqrt( ((point_list[0]-point_list[2])**2) + ((point_list[1]-point_list[3])**2))
 print(result)
 
+# Не поняла, куда добавить ограничение знаков после запятой?
